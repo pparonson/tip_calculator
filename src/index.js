93f4b3e92f7c2ed4dd95cd0fd4952d952b1efe0c
pparonson <preston.aronson@gmail.com>
2018-06-24 18:10:27 -0600

    initial boilerplate

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import update from "./update";
 
 
 const node  = document.getElementById("app");
-const el = document.createElement("");
+const el = document.createElement("p");
 el.innerHTML = JSON.stringify(initModel, null, 2);
 
 node.appendChild(el);
